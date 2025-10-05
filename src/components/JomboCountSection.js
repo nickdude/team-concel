06b8831ccdb67e270afd1dadf3a1338ff2127726
Manish Sharma <manishsharma@192.168.1.5>
2025-10-05 17:25:35 +0530

    count

--- src/components/JomboCountSection.js
+++ src/components/JomboCountSection.js
@@ -8,7 +8,7 @@ export default function JomboCountSection({ header, description, counts }) {
             <div className="w-full flex flex-col justify-center mb-16 px-4 sm:px-8 md:px-36 2xl:px-44">
                 <h1 className='text-left font-onest text-[2.13rem] font-semibold text-navy leading-[130%] tracking-[-0.67px]'>{header?.simple} <span className='text-navyAlpha font-ptserif font-normal italic'>{header?.italic}</span></h1>
                 <p className="font-onest text-brand-navy font-normal text-base leading-[27px] mt-3 w-[%]">{description}</p>
-                <div className="grid grid-cols-2 md:flex justify-between my-12 gap-2 md:space-x-5">
+                <div className="grid grid-cols-2 md:flex justify-between my-12 gap-4 md:space-x-5">
                     {counts.map((count, index) => (
                         <JomboCountCard
                             key={index}
